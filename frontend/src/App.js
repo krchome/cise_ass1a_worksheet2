f8de623b654b45efb617bc83cc261364248e11f5
krchome <krchome@hotmail.com>
2022-03-10 16:20:43 +1300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+/* function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to CISE - the home of learning and fun</h1>
+    </div>
+  );
+} */
+const App =()=><div className="App">
+<h1>Welcome to CISE - the home of learning and fun</h1>
+</div>
+
+export default App;
+
